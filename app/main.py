868bb69dcb5e49c9ea54e2d22bc1f585e9f1a190
Lautaro Farias <farias.lautaro002@gmail.com>
2024-04-16 23:22:48 -0300

    feat: Add socket 200 response

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ import socket
 
 def main() -> None:
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    connection, _client_address = server_socket.accept()  # wait for client
+    connection.send("HTTP/1.1 200 OK\r\n\r\n".encode())
 
 
 if __name__ == "__main__":
