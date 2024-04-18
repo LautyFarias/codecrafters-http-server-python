be43748e8190506d33891f23c15c6f9a2abe6bd4
Lautaro Farias <farias.lautaro002@gmail.com>
2024-04-17 21:04:17 -0300

    fix: Readd removed code

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
+import enum
 import socket
 
 ACCEPTED_BUFFSIZE = 1024
 """Accepted request buffer size by the socket server."""
 
-ECHO_PATH = "/echo/"
+
+class Route(enum.StrEnum):
+    """
+    Enumeration of available routes.
+    """
+
+    ROOT = "/"
+    ECHO = "/echo/"
 
 
 def main() -> None:
@@ -14,10 +22,13 @@ def main() -> None:
 
     _method, path, *_rest = buffer.decode().split()
 
-    if path.startswith(ECHO_PATH):
-        random_string = path.replace(ECHO_PATH, "", 1)
+    if Route.ROOT:
+        response = "HTTP/1.1 200 OK\r\n\r\n"
 
+    elif path.startswith(Route.ECHO):
+        random_string = path.replace(Route.ECHO, "", 1)
         response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n{random_string}"
+
     else:
         response = "HTTP/1.1 404 OK\r\n\r\n"
 
